579f693e574380faf646216c208239050c803b4c
Sachin Jaiswal <sj586997@gmail.com>
2026-06-08 12:23:47 +0530

    added a,b of CF

---
+++ Contests/CF 1102 Div-2 /A_Euclid,Sequence_and_Two_Numbers.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define f(i,k,n) for(int i = k; i < n; i++)
+#define fr(i,n,k) for(int i = n; i >= k; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define vb vector<bool>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mp make_pair
+#define take(arr) for(auto &x: arr) cin >> x;
+#define print(arr) for(auto x:arr) cout << x << " ";
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define nl "\n"
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const int MAXN = 2e5 + 5;
+const double EPS = 1e-9;
+
+void setup_io(string filename = "") {
+    if (!filename.empty()) {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+void sachin() {
+    ll n; cin >> n;
+    vl a(n); take(a);
+    sort(rall(a));
+    if (n == 2) {
+        cout << a[0] << " " << a[1] << nl;
+        return;
+    }
+    for (int i = 0; i + 2 < n; i++) {
+        if (a[i] % a[i + 1] != a[i + 2]) {
+            cout << -1 << nl;
+            return;
+        }
+    }
+    cout << a[0] << " " << a[1] << nl;
+}
+
+int main() {
+    fast_io;
+
+    // setup_io("test");
+    
+    int t = 1;
+    cin >> t;
+    while (t--) sachin();
+    return 0;
+}
