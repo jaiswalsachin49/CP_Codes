bf83ed0c4b4dd282cbe6d5f5a3a0844451ee1275
Sachin Jaiswal <sj586997@gmail.com>
2026-06-09 23:47:26 +0530

    added 1099 a,b

---
+++ Contests/CF 1099 Div-2/B_Another_Sorting_Problem.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define f(i,k,n) for(int i = k; i < n; i++)
+#define fr(i,n,k) for(int i = n; i >= k; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define vb vector<bool>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mp make_pair
+#define take(arr) for(auto &x: arr) cin >> x;
+#define print(arr) for(auto x:arr) cout << x << " ";
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define nl "\n"
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const int MAXN = 2e5 + 5;
+const double EPS = 1e-9;
+
+void setup_io(string filename = "") {
+    if (!filename.empty()) {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+/*
+
+*/
+
+void sachin() {
+    ll n; cin >> n;
+    vl arr(n); take(arr);
+    vl cpArr = arr;
+    ll val = -1;
+    f(i,1,n){
+        val = max(val,arr[i-1] - arr[i]);
+    }
+    for(int i = 1; i < n; i++){
+        if(arr[i] < cpArr[i-1]){
+            cpArr[i] += val;
+        }
+        if(cpArr[i] < cpArr[i-1]){
+            cout << "No" << nl;
+            return;
+        }
+    }
+    cout << "Yes" << nl;
+}
+
+int main() {
+    fast_io;
+
+    // setup_io("test");
+    
+    int t = 1;
+    cin >> t;
+    while (t--) sachin();
+    return 0;
+}
