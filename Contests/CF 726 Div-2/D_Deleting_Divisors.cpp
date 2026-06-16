1fafab5647170f8f1e31ebf60a50cca0d3b9e021
Sachin Jaiswal <sj586997@gmail.com>
2026-06-16 20:23:31 +0530

    16 June 2026

---
+++ Contests/CF 726 Div-2/D_Deleting_Divisors.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define f(i,k,n) for(int i = k; i < n; i++)
+#define fr(i,n,k) for(int i = n; i >= k; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define vb vector<bool>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mp make_pair
+#define take(arr) for(auto &x: arr) cin >> x;
+#define print(arr) for(auto x:arr) cout << x << " ";
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define nl "\n"
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const int MAXN = 2e5 + 5;
+const double EPS = 1e-9;
+
+void setup_io(string filename = "") {
+    if (!filename.empty()) {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+/*
+
+in odd case the bob will always take chance in even so alice will eventually end up in 1 which is not valid
+
+*/
+
+void sachin() {
+    ll n; cin >> n;
+    if(n % 2 == 1){
+        cout << "Bob" << nl;
+        return;
+    }
+    ll cnt = 0;
+    while(n % 2 == 0){
+        cnt++;
+        n /= 2;
+    }
+    if(n > 1) cout << "Alice" << nl;
+    else if(cnt % 2 == 0) cout << "Alice" << nl;
+    else cout << "Bob" << nl;
+}
+
+int main() {
+    fast_io;
+
+    // setup_io("test");
+    
+    int t = 1;
+    cin >> t;
+    while (t--) sachin();
+    return 0;
+}
